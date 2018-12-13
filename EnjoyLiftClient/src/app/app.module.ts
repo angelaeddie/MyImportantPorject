d6508ab0f2a41488cb33c135ab539e70a95d5ef0
angelaeddie <angelaeddie@163.com>
2018-12-14 04:05:58 +0800

    v1.00.01 tabs+menu

--- EnjoyLiftClient/src/app/app.module.ts
+++ EnjoyLiftClient/src/app/app.module.ts
@@ -1,37 +1,47 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+//根模块  告诉ionic怎么组装应用
+
+//引入angular和ionic的系统模块
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+//ionic 打包app以后配置启动画面，以及导航条的服务（不用管 ）
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+//引入根组件
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { OtherPage } from '../pages/other/other';
 
 @NgModule({
-  declarations: [
+  declarations: [ /*声明组件*/
     MyApp,
+    HomePage,
+    TabsPage,
     AboutPage,
     ContactPage,
-    HomePage,
-    TabsPage
+    OtherPage
   ],
-  imports: [
+  imports: [/*引入模块 依赖的模块*/
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
-  bootstrap: [IonicApp],
-  entryComponents: [
+  bootstrap: [IonicApp],/*启动的模块*/
+  entryComponents: [/*配置不会在模版中的组件*/
     MyApp,
+    HomePage,
+    TabsPage,
     AboutPage,
     ContactPage,
-    HomePage,
-    TabsPage
+    OtherPage
   ],
-  providers: [
+  providers: [/*配置服务*/
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- EnjoyLiftClient/src/pages/tabs/tabs.ts
+++ EnjoyLiftClient/src/pages/tabs/tabs.ts
@@ -1,19 +1,45 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
+import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+
+/**
+ * Generated class for the TabsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
 @Component({
-  templateUrl: 'tabs.html'
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html',
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tabRoots: Object[];
 
-  constructor() {
+  constructor(public navCtrl: NavController) {
 
+    this.tabRoots = [{
+      root: HomePage,
+      tabTitle: 'Home',
+      tabIcon: 'home'
+    },{
+      root: AboutPage,
+      tabTitle: 'About',
+      tabIcon: 'document'
+    },{
+      root: ContactPage,
+      tabTitle: 'Contact',
+      tabIcon: 'notifications'
+    }
+    ];
   }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TabsPage');
+  }
+
 }
